35f7eadbca18e2cdfef059364df2daca7f506468
Sarahribeiro5 <sarahribeiro.tds2.senai@gmail.com>
2025-09-02 11:54:08 -0300

    feat: add things

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-    title: "escreva o TÍTULO para aparecer na aba do navegador",
+    title: "switch games",
     description: "Projeto pra mostrar tudo que eu sei",
     icons: {
         icon: "/icons/favicon.ico",
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 "use client"
 import React from 'react'
+import Image from 'next/image'
 import { useState } from 'react'
 import axios from 'axios'
 
@@ -22,28 +23,41 @@ export default function Page() {
   }
 
   return (
-    <div className="min-h screen bg-blue-100 p-8 text-center">
-      <div className="mx-auto text-center mb-12">
-        <h1 className="text-center mb-8 font-bold">Jogos</h1>
-        <div className="text-center mb-8">
-          <div className="mm-6">
-            <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={buscarJogos} disabled={loading}>
-              {loading ? "Carregando..." : "Buscar Jogos"}
-            </button>
-          </div>
+    <div className="min-h-screen bg-blue-100 p-8 text-center">
+      {/* Informações obrigatórias */}
+      <div className="mb-12">
+        <h1 className="text-4xl mb-4">Turma: 2TDS2</h1>
+        <h2 className="text-2xl mb-2">Escola: SENAI Valinhos</h2>
+        <h3 className="text-xl mb-4">Aluno: Sarah Ribeiro Barros</h3>
+        <div className="flex justify-center mb-4">
+          <Image
+            src="/avatar.jpg" 
+            alt="Foto do Aluno"
+            width={150}
+            height={150}
+            className="rounded-full"
+          />
         </div>
+        <p className="italic text-lg">"O sucesso é a soma de pequenos esforços repetidos dia após dia." - Robert Collier</p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+      {/* Botão para buscar jogos */}
+      <div className="text-center mb-12">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={buscarJogos} disabled={loading}>
+          {loading ? "Carregando..." : "Buscar Jogos"}
+        </button>
+      </div>
+
+      {/* Lista de jogos */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {jogos.map((jogo) => (
           <div className="bg-white p-6 rounded-lg shadow-md" key={jogo.id}>
             <h3 className="font-bold text-lg text-gray-800">{jogo.name}</h3>
-            <p className="text-grey-600">Gênero: {jogo.genre}</p>
-            <p className="text-grey-600">Desenvolvedores: {jogo.developers}</p>
+            <p className="text-gray-600">Gênero: {jogo.genre}</p>
+            <p className="text-gray-600">Desenvolvedores: {jogo.developers}</p>
           </div>
         ))}
       </div>
-
     </div>
   )
 }
